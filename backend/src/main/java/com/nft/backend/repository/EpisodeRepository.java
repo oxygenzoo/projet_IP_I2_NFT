384ede487cb0b976ff2b99db54445120d1b1cd80
enzoguignabert <enzo.guignabert91@gmail.com>
2026-06-25 00:33:15 +0200

    model episode

---
+++ backend/src/main/java/com/nft/backend/repository/EpisodeRepository.java
@@ -0,0 +1,12 @@
+package com.nft.backend.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.nft.backend.model.Episode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EpisodeRepository extends JpaRepository<Episode, UUID> {
+
+    List<Episode> findByTravelIdOrderByEpisodeNumberAsc(UUID travelId);
+}
